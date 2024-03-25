aaae1ab50c0c0e64e5c1cd79a5e2b1fc1adb0d02
Tae Jeong Kim <oganesson12@hufs.ac.kr>
2024-03-25 12:44:16 +0900

    Title: 함수를 이용한 소수 판별, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240325/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+
+def sosu(c):
+    for i in range(2, c):
+        if c % i == 0:
+            return False
+        else:
+            continue
+    return True
+
+def sumab(a,b):
+    sum1 = 0
+    for j in range(a, b+1):
+        if sosu(j) == True:
+            sum1 = sum1 + j
+        else:
+            continue
+    return sum1
+
+print(sumab(a,b))
